2c6098c6f4f138844113c1138b2f70adf61a6385
nerdkps <krusheepriya@gmail.com>
2022-03-07 10:04:12 +0530

    first commit

---
+++ Sample_test.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+import os
+
+print(os.getcwd())
+
+"""
+driver=webdriver.Chrome(executable_path=r"C:\\Users\\user\\PycharmProjects\\pythonProject\\chromedriver\\chromedriver.exe")
+driver.get("https://www.amazon.com")
+driver.maximize_window()
+
+driver.close()
+"""
+
+list=['s','r','d']
+
+for i in list:
+    print(i)
+
